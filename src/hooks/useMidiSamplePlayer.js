858e7048279dbccb076889e529ba99a5decf39c3
Simon Wiles <simonjwiles@gmail.com>
2020-09-17 08:25:05 -0700

    Expose slider tempo adjustment in useMidiSamplePlayer hook

--- src/hooks/useMidiSamplePlayer.js
+++ src/hooks/useMidiSamplePlayer.js
@@ -19,32 +19,31 @@ const decodeHtmlEntities = (string) =>
       String.fromCodePoint(parseInt(num, 16)),
     );
 
+const midiSamplePlayer = new MidiPlayer.Player();
+
+const activeAudioNodes = {};
+const activeNotes = [];
+const sustainedNotes = [];
+
+let adsr = ADSR_SAMPLE_DEFAULTS;
+let baseTempo;
+let tempoRatio = 1.0; // To keep track of gradual roll acceleration
+let sliderTempo = 60.0;
+
+const volumeRatio = 1.0;
+const leftVolumeRatio = 1.0;
+const rightVolumeRatio = 1.0;
+const sustainPedalOn = false;
+const softPedalOn = false;
+const panBoundary = HALF_BOUNDARY;
+const sampleInst = "acoustic_grand_piano";
+
 const useMidiSamplePlayer = (mididata) => {
-  const [midiSamplePlayer, setMidiSamplePlayer] = useState();
   const [rollMetadata, setRollMetadata] = useState({});
 
-  const activeAudioNodes = {};
-  const activeNotes = [];
-  const sustainedNotes = [];
-
-  let adsr = ADSR_SAMPLE_DEFAULTS;
-  let baseTempo;
-  let tempoRatio = 1.0; // To keep track of gradual roll acceleration
-  let playbackTempo = 0.0; // Combines slider and tempo ratio
-  let sliderTempo = 60.0;
-
-  const volumeRatio = 1.0;
-  const leftVolumeRatio = 1.0;
-  const rightVolumeRatio = 1.0;
-  const sustainPedalOn = false;
-  const softPedalOn = false;
-  const panBoundary = HALF_BOUNDARY;
-  const sampleInst = "acoustic_grand_piano";
-
   const initMidiPlayer = (instrument, audioContext) => {
-    const _midiSamplePlayer = new MidiPlayer.Player();
-    _midiSamplePlayer.on("fileLoaded", () => {
-      const metadataTrack = _midiSamplePlayer.events[0];
+    midiSamplePlayer.on("fileLoaded", () => {
+      const metadataTrack = midiSamplePlayer.events[0];
       setRollMetadata(
         /* @IMAGE_WIDTH and @IMAGE_LENGTH should be the same as from viewport._contentSize
          * Can't think of why they wouldn't be, but maybe check anyway. Would need to scale
@@ -77,94 +76,92 @@ const useMidiSamplePlayer = (mididata) => {
       ).reduce((prev, curr) => (prev.tick < curr.tick ? prev : curr)).data;
     });
 
-    _midiSamplePlayer.on(
-      "midiEvent",
-      ({ name, data, noteNumber, velocity }) => {
-        if (name === "Note on") {
-          if (velocity === 0) {
-            // Note off
-            if (
-              noteNumber in activeAudioNodes &&
-              !sustainedNotes.includes(noteNumber)
-            ) {
-              try {
-                activeAudioNodes[noteNumber].stop();
-              } catch (ex) {
-                console.log(
-                  "COULDN'T STOP NOTE, PROBABLY DUE TO WEIRD ADSR VALUES, RESETTING",
-                );
-
-                adsr = ADSR_SAMPLE_DEFAULTS;
-              }
-              // activeAudioNodes[noteNumber] = null;
-              // delete activeAudioNodes[noteNumber];
-            }
-            // remove every occurrence of noteNumber in activeNotes
-            while (activeNotes.includes(noteNumber)) {
-              activeNotes.splice(activeNotes.indexOf(noteNumber), 1);
-            }
-          } else {
-            // Note on
-            if (sustainedNotes.includes(noteNumber)) {
-              try {
-                activeAudioNodes[noteNumber].stop();
-              } catch (ex) {
-                console.log(
-                  "Tried and failed to stop sustained note being re-touched",
-                  noteNumber,
-                );
-              }
-              activeAudioNodes[noteNumber] = null;
-            }
-
-            let updatedVolume = (velocity / 100) * volumeRatio;
-            if (softPedalOn) {
-              updatedVolume *= SOFT_PEDAL_RATIO;
-            }
-            if (noteNumber < panBoundary) {
-              updatedVolume *= leftVolumeRatio;
-            } else if (noteNumber >= panBoundary) {
-              updatedVolume *= rightVolumeRatio;
-            }
-
+    midiSamplePlayer.on("midiEvent", ({ name, data, noteNumber, velocity }) => {
+      if (name === "Note on") {
+        if (velocity === 0) {
+          // Note off
+          if (
+            noteNumber in activeAudioNodes &&
+            !sustainedNotes.includes(noteNumber)
+          ) {
             try {
-              activeAudioNodes[noteNumber] = instrument.play(
-                noteNumber,
-                audioContext.currentTime,
-                {
-                  gain: updatedVolume,
-                  adsr,
-                },
-              );
+              activeAudioNodes[noteNumber].stop();
             } catch (ex) {
-              // Get rid of this eventually
-              console.log("IMPOSSIBLE ADSR VALUES FOR THIS NOTE, RESETTING");
+              console.log(
+                "COULDN'T STOP NOTE, PROBABLY DUE TO WEIRD ADSR VALUES, RESETTING",
+                ex,
+              );
+
               adsr = ADSR_SAMPLE_DEFAULTS;
-              activeAudioNodes[noteNumber] = instrument.play(
+            }
+            delete activeAudioNodes[noteNumber];
+          }
+          // remove every occurrence of noteNumber in activeNotes
+          while (activeNotes.includes(noteNumber)) {
+            activeNotes.splice(activeNotes.indexOf(noteNumber), 1);
+          }
+        } else {
+          // Note on
+          if (sustainedNotes.includes(noteNumber)) {
+            try {
+              activeAudioNodes[noteNumber].stop();
+            } catch (ex) {
+              console.log(
+                "Tried and failed to stop sustained note being re-touched",
                 noteNumber,
-                audioContext.currentTime,
-                {
-                  gain: updatedVolume,
-                  adsr,
-                },
               );
             }
-            if (sustainPedalOn && !sustainedNotes.includes(noteNumber)) {
-              sustainedNotes.push(noteNumber);
-            }
-            if (!activeNotes.includes(noteNumber)) {
-              activeNotes.push(noteNumber);
-            }
+            activeAudioNodes[noteNumber] = null;
           }
-        } else if (name === "Set Tempo") {
-          tempoRatio = 1 + (data - baseTempo) / baseTempo;
-          playbackTempo = sliderTempo * tempoRatio;
-          _midiSamplePlayer.setTempo(playbackTempo);
+
+          let updatedVolume = (velocity / 100) * volumeRatio;
+          if (softPedalOn) {
+            updatedVolume *= SOFT_PEDAL_RATIO;
+          }
+          if (noteNumber < panBoundary) {
+            updatedVolume *= leftVolumeRatio;
+          } else if (noteNumber >= panBoundary) {
+            updatedVolume *= rightVolumeRatio;
+          }
+
+          try {
+            activeAudioNodes[noteNumber] = instrument.play(
+              noteNumber,
+              audioContext.currentTime,
+              {
+                gain: updatedVolume,
+                adsr,
+              },
+            );
+          } catch (ex) {
+            // Get rid of this eventually
+            console.log("IMPOSSIBLE ADSR VALUES FOR THIS NOTE, RESETTING");
+            adsr = ADSR_SAMPLE_DEFAULTS;
+            activeAudioNodes[noteNumber] = instrument.play(
+              noteNumber,
+              audioContext.currentTime,
+              {
+                gain: updatedVolume,
+                adsr,
+              },
+            );
+          }
+          if (sustainPedalOn && !sustainedNotes.includes(noteNumber)) {
+            sustainedNotes.push(noteNumber);
+          }
+          if (!activeNotes.includes(noteNumber)) {
+            activeNotes.push(noteNumber);
+          }
         }
-      },
-    );
-    _midiSamplePlayer.loadDataUri(mididata);
-    setMidiSamplePlayer(_midiSamplePlayer);
+      } else if (name === "Set Tempo") {
+        // Note: MidiPlayerJS automatically sets the tempo in response to
+        //       "Set Tempo" events, so we need to intervene here.
+        // (https://github.com/grimmdude/MidiPlayerJS/releases/tag/2.0.12)
+        tempoRatio = 1 + (data - baseTempo) / baseTempo;
+        midiSamplePlayer.setTempo(sliderTempo * tempoRatio);
+      }
+    });
+    midiSamplePlayer.loadDataUri(mididata);
   };
 
   const playPauseMidiFile = () => {
@@ -177,6 +174,17 @@ const useMidiSamplePlayer = (mididata) => {
 
   const stopMidiFile = () => {
     midiSamplePlayer.stop();
+  };
+
+  const updateSliderTempo = (_sliderTempo) => {
+    // If not paused during tempo change, player jumps back a bit on
+    // shift to slower playback tempo, forward on shift to faster tempo.
+    // See https://github.com/grimmdude/MidiPlayerJS/issues/13#issuecomment-354489911
+    midiSamplePlayer
+      .pause()
+      .setTempo(_sliderTempo * tempoRatio)
+      .play();
+    sliderTempo = _sliderTempo;
   };
 
   useEffect(() => {
@@ -194,7 +202,13 @@ const useMidiSamplePlayer = (mididata) => {
     }).then((instrument) => initMidiPlayer(instrument, audioContext));
   }, []);
 
-  return { rollMetadata, playPauseMidiFile, stopMidiFile };
+  return {
+    rollMetadata,
+    playPauseMidiFile,
+    stopMidiFile,
+    sliderTempo,
+    updateSliderTempo,
+  };
 };
 
 export default useMidiSamplePlayer;
